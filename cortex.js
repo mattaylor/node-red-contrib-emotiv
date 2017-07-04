a569598250230f12a4fa84e3c237f979b89ddc68
mat <mat@emotiv.com>
2017-07-04 11:15:02 +0700

    typos

--- cortex.js
+++ cortex.js
@@ -1,11 +1,10 @@
 var Emocore = require("emocore")
-
 var streams = ['dev', 'eeg', 'cog', 'mot', 'fac', 'pow']
 
 function Emotiv(config) {
 	RED.nodes.createNode(this,config)
 	var cortex = new Emocore(this.credentials).newSession()
-   this.on('input', msg => if (msg.jsonrpc) cortex.call(msg.method, msg.params))
+   	this.on('input', msg => if (msg.jsonrpc) cortex.call(msg.method, msg.params))
 	this.on('close', msg => cortex.set({status:'closed'})
 	streams.map(stream => if (this[stream]) cortex.on(stream, msg => this.send(msg)))
 }
@@ -13,8 +12,8 @@ function Emotiv(config) {
 var emoCreds = { 
 	username: {type:"text"}, 
   	password: {type:"password"}, 
-	license: {type:"text" } 
-	client_id: {type:"text"}
+	license: {type:"text" }, 
+	client_id: {type:"text"},
 	client_secret: {type:"text"}
 }
 
